7b8196639ca0555a5dd3ca57b2322279fe7bf2fc
RoxanaLarisssa <roxana.larisssa@gmail.com>
2023-10-31 11:04:22 +0000

    update js

--- data-time.js
+++
@@ -1,7 +0,0 @@
-let hrs = document.getElementById("hrs");
-let min = document.getElementById("min");
-let sec = document.getElementById("sex");
-
-let currentTime = new Date();
-
-hrs.innerHTML = currentTime;
